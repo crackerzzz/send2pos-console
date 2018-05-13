f40a0aae34583c438c73b7a67a0112d3effeb772
crackerzzz <shreejwal.shrestha@gmail.com>
2018-05-14 09:53:48 +1000

    Built json viewer

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -7,42 +7,14 @@ import {
     TableRow,
     TableRowColumn,
 } from 'material-ui/Table';
-import RaisedButton from 'material-ui/RaisedButton';
-import FlatButton from 'material-ui/FlatButton';
-import Dialog from 'material-ui/Dialog';
-import { TextField } from 'material-ui';
-import JSONPretty from 'react-json-pretty';
+import JsonViewer from './JsonViewer';
 
 class SearchResults extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            requestPayloadOpen: false,
-            responsePayloadOpen: false,
-        };
     }
 
-    handleRequestOpen = () => this.setState({ requestPayloadOpen: true })
-    handleRequestClose = () => this.setState({ requestPayloadOpen: false })
-
-    handleResponseOpen = () => this.setState({ responsePayloadOpen: true })
-    handleResponseClose = () => this.setState({ responsePayloadOpen: false })
-
     render() {
-        const requestActions = [
-            <FlatButton
-                label="Ok"
-                primary={true}
-                onClick={this.handleRequestClose}
-            />
-        ];
-        const responseActions = [
-            <FlatButton
-                label="Ok"
-                primary={true}
-                onClick={this.handleResponseClose}
-            />
-        ];
         return (
             <Table>
                 <TableHeader>
@@ -65,29 +37,10 @@ class SearchResults extends React.Component {
                             <TableRowColumn>{result.date}</TableRowColumn>
                             <TableRowColumn>{result.status}</TableRowColumn>
                             <TableRowColumn>
-                                <RaisedButton label="Open" onClick={this.handleRequestOpen} />
-                                <Dialog
-                                    title="Request Payload"
-                                    actions={requestActions}
-                                    modal={false}
-                                    open={this.state.requestPayloadOpen}
-                                    onRequestClose={this.handleRequestClose}
-                                    autoScrollBodyContent={true}
-                                >
-                                    <JSONPretty id="json-pretty" json={result.requestPayload}></JSONPretty>
-                                </Dialog>
+                                <JsonViewer title="Request Payload" json={result.requestPayload} />
                             </TableRowColumn>
                             <TableRowColumn>
-                                <RaisedButton label="Open" onClick={this.handleResponseOpen} />
-                                <Dialog
-                                    title="Response Payload"
-                                    actions={responseActions}
-                                    modal={false}
-                                    open={this.state.responsePayloadOpen}
-                                    onRequestClose={this.handleResponseClose}
-                                >
-                                    <JSONPretty id="json-pretty" json={result.responsePayload}></JSONPretty>
-                                </Dialog>
+                                <JsonViewer title="Response Payload" json={result.responsePayload} />
                             </TableRowColumn>
                         </TableRow>
                     )}
